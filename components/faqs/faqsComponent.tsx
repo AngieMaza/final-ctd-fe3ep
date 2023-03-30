2555cf17ecd720c78179fac1c68eca61739c5149
Angeles Maza <angie.maza.89@gmail.com>
2023-03-29 21:15:39 -0300

    First commit before Vercel

---
+++ components/faqs/faqsComponent.tsx
@@ -0,0 +1,5 @@
+const Faqs = () => {
+    
+};
+
+export default Faqs;
--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -2,16 +2,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html style={{height: '100%'}}>
+        <Html style={{ height: '100%' }}>
             <Head>
                 <link
-                    href="https://fonts.googleapis.com/css2?family=Roboto"
+                    href="https://fonts.googleapis.com/css2?family=Roboto&display=optional"
                     rel="stylesheet"
                 />
+                <link rel="icon" href="/marvel-icon.png" />
             </Head>
-            <body style={{height: '100%'}}>
-            <Main />
-            <NextScript />
+            <body style={{ height: '100%' }}>
+                <Main />
+                <NextScript />
             </body>
 
         </Html>
